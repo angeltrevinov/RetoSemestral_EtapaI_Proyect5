6b9690d47c14ae4b9da09c7058b3878010155175
Angel Odiel Trevino Villanueva <angel.trevinov@hotmail.com>
2017-10-14 15:09:53 -0500

    Starting  5th project

--- RetoSemestral_EtapaI_Proyect5/main.cpp
+++ RetoSemestral_EtapaI_Proyect5/main.cpp
@@ -1,15 +1,34 @@
-//
-//  main.cpp
-//  RetoSemestral_EtapaI_Proyect5
-//
-//  Created by Angel Trevino on 10/14/17.
-//  Copyright © 2017 Angel Odiel Treviño Villanueva. All rights reserved.
-//
-
+/*
+    Angel Odiel Treviño Villanueva
+    A01336559
+    Estructura de Datos
+    Reto 5
+    Game of Two Stacks
+ */
+#include <vector>
 #include <iostream>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+
+int main(){
+    int g;
+    cin >> g;
+    for(int a0 = 0; a0 < g; a0++){
+        int n;
+        int m;
+        int x;
+        cin >> n >> m >> x;
+        vector<int> a(n);
+        for(int a_i = 0; a_i < n; a_i++){
+            cin >> a[a_i];
+        }
+        vector<int> b(m);
+        for(int b_i = 0; b_i < m; b_i++){
+            cin >> b[b_i];
+        }
+        // your code goes here
+        int CantMov = 0;
+        
+    }
     return 0;
 }
